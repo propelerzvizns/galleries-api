930a1c9225938f079c5a39205799d70c953d853a
propelerzvizns <propelerzvizns@gmail.com>
2020-11-02 21:13:13 +0100

    created migrations for galleries, images,comments and reworked users migration, but i didn't migrate migrations to database

--- database/migrations/2020_11_02_181911_create_image_table.php
+++ database/migrations/2020_11_02_181911_create_image_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateImageTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('image', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('img_url');
+            $table->foreignId('gallery_id');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('image');
     }
 }
