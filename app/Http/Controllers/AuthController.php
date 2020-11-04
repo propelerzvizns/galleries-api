376d1e8bc97ee4c50123cf595875951104d833e0
Dusan <dusanjankovic877@gmail.com>
2020-11-04 15:29:12 +0100

    implemented UserLoginRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,23 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserLoginRequest;
 use Illuminate\Support\Facades\Auth;
 // use App\Http\Controllers\Controller;
+use Illuminate\Support\MessageBag;
 
 class AuthController extends Controller
 {
     //
 
-    public function login(Request $request){
-       
+    public function login(UserLoginRequest $request){
+       $validated = $request->validated();
+
         $credentials = [
-            'email' => $request->get('email'),
-            'password' => $request->get('password')
+            'email' => $validated['email'],
+            'password' => $validated['password']
         ];
         $token = auth()->attempt($credentials);
-        
+
         info($token);
         if(!$token){
+
             return response()->json(['error' => 'Unauthorized'], 401);
         }
         return ['token' => $token , 'user' => auth()->user()];
